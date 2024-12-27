ef4a38b37c8d886f0fe404846a160cd1de7a7215
kimhyunso <gustn9515@gmail.com>
2024-12-27 18:02:35 +0900

    feat: class deep copy

---
+++ src/chapter3/Main.java
@@ -0,0 +1,38 @@
+package chapter3;
+
+class MyString {
+    private byte[] string;
+
+    public MyString(String word) {
+        this.string = word.getBytes();
+    }
+
+    public MyString(MyString rhs) {
+        deepCopy(rhs);
+    }
+
+    public void deepCopy(MyString rhs) {
+        rhs.string = this.string;
+    }
+
+    public void setString(String string) {
+        this.string = string.getBytes();
+    }
+
+    public String getString() {
+        if (string == null || string.length == 0) {
+            return "(null)";
+        }
+        return new String(string);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        MyString src = new MyString("Hello");
+        MyString dst = new MyString(src);
+
+        src.setString("World");
+        System.out.println(dst.getString());
+    }
+}
