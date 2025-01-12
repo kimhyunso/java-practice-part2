3e225288334afe61d0135b1a78ca3155136047ba
kimhyunso <gustn9515@gmail.com>
2025-01-12 17:28:23 +0900

    feat: JAVAP

---
+++ src/chapter5/Main2.java
@@ -0,0 +1,9 @@
+package chapter5;
+
+public class Main2 {
+    public static void main(String[] args) {
+        // \out\production\java-practice-part2\chapter5
+        // javap -v Main2
+        System.out.println("Hello World");
+    }
+}
