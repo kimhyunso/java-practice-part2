af199d896c7b12120a5be9caf1af7207b5586a0c
kimhyunso <gustn9515@gmail.com>
2025-03-11 18:24:49 +0900

    feat: 챕터 7 실습 기능 구현

---
+++ src/chapter7/Main1.java
@@ -0,0 +1,40 @@
+package chapter7;
+
+class MyString {
+    protected byte[] string;
+
+    public void setString(String string) {
+        this.string = onSetStringReplace(string).getBytes().clone();
+    }
+    protected boolean onSetString(String string) {
+        return true;
+    }
+
+    protected String onSetStringReplace(String param) {
+        return param;
+    }
+
+    public String getString() {
+        if (string == null)
+            return "(null)";
+        return new String(string);
+    }
+
+    public boolean equals(MyString rhs) {
+        String temp = new String(this.string);
+        // return temp.equals(new String(rhs.string));
+        return temp.compareTo(rhs.getString()) == 0;
+    }
+}
+
+
+public class Main1 {
+    public static void main(String[] args) {
+        MyString s1 = new MyString();
+        s1.setString("Hello");
+        MyString s2 = new MyString();
+        s2.setString("Hello");
+
+        System.out.println(s1.equals(s2));
+    }
+}
